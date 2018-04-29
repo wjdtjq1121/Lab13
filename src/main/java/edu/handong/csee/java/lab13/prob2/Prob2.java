69fb57cd60b13c217475740b9680e0a7172b9ccc
JeongSeop <wjdtjq1121@gmail.com>
2018-04-29 16:18:46 +0900

    done prob 2

--- src/main/java/edu/handong/csee/java/lab13/prob2/Prob2.java
+++ src/main/java/edu/handong/csee/java/lab13/prob2/Prob2.java
@@ -2,12 +2,16 @@ package edu.handong.csee.java.lab13.prob2;
 
 public class Prob2 {
 
-	public Prob2() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Book book = new Book("Simple Book");
+		Science science = new Science("Hello Physics", "ScienceWorld"); // Name, publisher
+		History history1 = new History("What Is history?", "E.H.Brian"); //Name, Author
+		
+		book.show();
+		science.show();
+		history1.show();
+		
+		
 
 	}
 
