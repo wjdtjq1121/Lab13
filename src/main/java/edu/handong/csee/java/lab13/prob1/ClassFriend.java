43d32cf8d7eae7e3592d034b8e0f2bf2bcc9d341
JeongSeop <wjdtjq1121@gmail.com>
2018-05-01 17:04:36 +0900

    add single comment class friend

--- src/main/java/edu/handong/csee/java/lab13/prob1/ClassFriend.java
+++ src/main/java/edu/handong/csee/java/lab13/prob1/ClassFriend.java
@@ -1,11 +1,11 @@
-package edu.handong.csee.java.lab13.prob1;
+package edu.handong.csee.java.lab13.prob1; // set the package where this belongs to
 
-import java.util.Scanner;
+import java.util.Scanner; // get the scanner class from java.util library
 
-public class ClassFriend extends SchoolFriend {
+public class ClassFriend extends SchoolFriend { // a class which can access from other package
 
-	public void classFriend() {
-		System.out.println("Class Friend!");
+	public void classFriend() { // a void method of classfriend no return value.
+		System.out.println("Class Friend!"); // print the statement class friend.
 	}
 
 }
