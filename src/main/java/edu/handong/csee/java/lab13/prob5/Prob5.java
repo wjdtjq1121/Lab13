9609333a463f2629775c0929174400ca301ca4a9
JeongSeop <wjdtjq1121@gmail.com>
2018-05-01 16:16:50 +0900

    made boolean equals method prob 5

---
+++ src/main/java/edu/handong/csee/java/lab13/prob5/Prob5.java
@@ -0,0 +1,14 @@
+package edu.handong.csee.java.lab13.prob5;
+
+public class Prob5 {
+
+	public Prob5() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
